648c2de6c6dcfd0c432b55bd12fcbde53c481544
Kostya <kostikbushmelev@gmail.com>
2025-05-12 14:04:15 +0300

    add csv reader

--- src/main/java/liptsoft/microservice/MicroserviceApplication.java
+++ src/main/java/liptsoft/microservice/MicroserviceApplication.java
@@ -1,13 +1,25 @@
 package liptsoft.microservice;
 
+import liptsoft.microservice.model.Transaction;
+import liptsoft.microservice.model.Type;
+import liptsoft.microservice.util.CsvTransactionReader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class MicroserviceApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceApplication.class, args);
+		
+		CsvTransactionReader transactionReader = new CsvTransactionReader();
+		List<Transaction> transactions = transactionReader.readTransactions("src/main/resources/data/incomes.csv", Type.INCOME);
+		for (int i = 0; i < 10; ++i) {
+			System.out.println(transactions.get(i).getTransactionId());
+		}
 	}
 	
 }
---
+++ src/main/java/liptsoft/microservice/util/CsvTransactionReader.java
@@ -0,0 +1,48 @@
+package liptsoft.microservice.util;
+
+import liptsoft.microservice.builder.TransactionBuilder;
+import liptsoft.microservice.model.Transaction;
+import liptsoft.microservice.model.Type;
+
+import java.io.FileReader;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class CsvTransactionReader {
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+
+    public List<Transaction> readTransactions(String path, Type type) {
+        try (Scanner scanner = new Scanner(new FileReader(path))) {
+            if (scanner.hasNextLine()) scanner.nextLine();
+
+            return scanner.tokens()
+                    .map(line -> parseLine(line, type))
+                    .filter(Objects::nonNull)
+                    .toList();
+
+        } catch (Exception e) {
+            throw new RuntimeException("Ошибка чтения файла: " + path, e);
+        }
+    }
+
+    private Transaction parseLine(String line, Type type) {
+        try {
+            String[] tokens = line.split(",");
+            return new TransactionBuilder()
+                    .setTransactionId(Long.parseLong(tokens[0]))
+                    .setCustomerId(Integer.parseInt(tokens[1]))
+                    .setAccountId(Long.parseLong(tokens[2]))
+                    .setAmount(new BigDecimal(tokens[3]))
+                    .setDateTime(LocalDateTime.parse(tokens[4], FORMATTER))
+                    .setType(type)
+                    .build();
+
+        } catch (Exception e) {
+            System.err.println("Ошибка при парсинге строки: " + line);
+            return null;
+        }
+    }
+}
